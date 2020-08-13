174d9fb4f5434c4a32c40b297fe085f8e8ac95bd
Omar Jbara <omar.jbara2@gmail.com>
2020-08-13 11:32:19 +0200

    - Removed isomorphic fetch since Next@9.4 auto provides fetch

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,4 +1,3 @@
-import fetch from 'isomorphic-fetch'
 import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
